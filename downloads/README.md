f6c8273e3c5019a8475c7c3ccbc9c733d23ecf98
brt crt <bartuturan05@gmail.com>
2021-04-28 23:29:07 +0300

    Colorization with chalk. Also switched from nothing to commander.

---
+++ downloads/README.md
@@ -0,0 +1,3 @@
+# This is where the downloaded videos go.
+
+You need to have this folder or else the program will just outright crash.
--- index.js
+++ index.js
@@ -1,29 +1,110 @@
 #! /usr/bin/env node
 
+// imports
 const ytdl = require("ytdl-core");
+const chalk = require("chalk");
 const fs = require("fs");
+const path = require("path");
+const { Command } = require("commander");
+// imports
 
-const url = process.argv.slice(2)[0];
-var audio_only = process.argv.slice(2)[1];
+const cli = new Command()
+  .option("-u, --url <url>", "Give a youtube link.")
+  .option("-a, --audio", "Only download audio")
+  .parse(process.argv);
 
-if (audio_only === "true") {
-  audio_only = true;
-} else if (audio_only === "false") {
+// variables
+const options = cli.opts();
+const url = options.url;
+let audio_only = true;
+// variables
+
+if (!options["audio"]) {
+  // if the user didn't say -a or --audio
   audio_only = false;
 }
 
-if (!url) throw "No url given.";
-const main = async (url, audio_only = false) => {
-  console.log(audio_only);
+if (!url) {
+  // help command type of thing
+  console.log();
+  console.log(`
+                    ${
+                      chalk.redBright("yt") +
+                      chalk.greenBright("-") +
+                      chalk.blueBright("mp3") +
+                      chalk.greenBright("-") +
+                      chalk.redBright("mp4")
+                    }
+
+  
+  ${chalk.gray("Github: https://github.com/brtcrt/yt-mp3-mp4")}
+
+
+  ${chalk.magenta("Parameters you can pass are:")}
+  ${chalk.blue("-u / --url <youtube url> ")}${chalk.greenBright(
+    "-->"
+  )}${chalk.whiteBright(
+    " The youtube video which you want to download. You need to specify this."
+  )}
+  ${chalk.blue("-a / --audio ")}${chalk.greenBright("-->")}${chalk.whiteBright(
+    " Use this if you just want to download the audio."
+  )}${chalk.red(" Don't use this if you want both the video and the audio!")}
+  
+  ${chalk.magenta("Here are some examples:")}
+  ${chalk.green(
+    "yt-convert -u https://www.youtube.com/watch?v=4TkFmmdmk50 -a"
+  )}${chalk.whiteBright(" Only download the audio.(.mp3)")}
+  ${chalk.green(
+    "yt-convert -u https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  )}${chalk.whiteBright(" Downloads both the video and the audio.(.mp4)")}
+  
+  `);
+  return;
+} else {
+  isValid = ytdl.validateURL(url);
+  if (!isValid) {
+    console.log(`
+    ${chalk.red("Couldn't find a youtube video with that url.")}
+  `);
+    return;
+  }
+  console.log(`
+  ${chalk.green("Found the video!")}
+  `);
+}
+
+const download = async (url, audio_only) => {
   results = await ytdl.getInfo(url);
-  console.log(
-    `Downloading ${audio_only ? "mp3" : "mp4"} of ${results.videoDetails.title}`
-  );
+  console.log(`
+  ${chalk.cyan(
+    `Downloading ${results.videoDetails.title.trim()} as ${
+      audio_only ? "mp3" : "mp4"
+    }`
+  )}
+  `);
   ytdl(url, { filter: audio_only ? "audioonly" : null }).pipe(
     fs.createWriteStream(
-      `./downloads/${results.videoDetails.title}.${audio_only ? "mp3" : "mp4"}`
+      path.join(
+        __dirname,
+        `./downloads/${results.videoDetails.title.trim()}.${
+          audio_only ? "mp3" : "mp4"
+        }`
+      )
     )
   );
+  return path.join(
+    __dirname,
+    `./downloads/${results.videoDetails.title.trim()}.${
+      audio_only ? "mp3" : "mp4"
+    }`
+  );
+};
+
+const main = async (url, audio_only) => {
+  const file_location = await download(url, audio_only);
+  console.log(`
+  ${chalk.green(`Done! The file is at ${chalk.magenta(file_location)}`)}
+  `);
 };
 
 main(url, audio_only);
